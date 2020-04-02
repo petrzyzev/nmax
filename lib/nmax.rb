58713ac662de4ce8f753d3fc7c67d46e679359f5
Petr Zyuzev <petrzyzev@gmail.com>
2020-04-02 21:05:58 +0300

    changed constant

--- lib/nmax.rb
+++ lib/nmax.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 
 require 'nmax/version'
 
@@ -27,7 +26,7 @@ module Nmax
         byte = io.getbyte
         break if byte.nil?
 
-        number = required_bytes[byte]
+        number = REQUIRED_BYTES[byte]
         if number
           result_array.last << number
         else
