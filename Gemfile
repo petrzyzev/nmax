c9a242ea80b3ba11b88266d2cd227ea02b08e767
Petr Zyuzev <petrzyzev@gmail.com>
2020-04-02 18:47:51 +0300

    start project

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in nmax.gemspec
+gemspec
+
+gem "rake", "~> 12.0"
---
+++ lib/nmax.rb
@@ -0,0 +1,31 @@
+
+require 'nmax/version'
+
+module Nmax
+  class Error < StandardError; end
+  # Your code goes here...
+  #puts $stdin.each_line.with_object([]) { |l, ns| l.scan(/\d+/) { |n| ns << n.to_i } }.uniq.sort.reverse[0...ARGV[0].to_i]
+  required_bytes = { 48 => '0',
+                     49 => '1',
+                     50 => '2',
+                     51 => '3',
+                     52 => '4',
+                     53 => '5',
+                     54 => '6',
+                     55 => '7',
+                     56 => '8',
+                     57 => '9' }
+  result_array = ['']
+  loop do
+    byte = $stdin.getbyte
+    break if byte.nil?
+
+    number = required_bytes[byte]
+    if number
+      result_array.last << number
+    else
+      result_array << '' unless result_array.last.empty?
+    end
+  end
+  puts result_array.sort_by(&:to_i).reverse[0...ARGV[0].to_i]
+end
